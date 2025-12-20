76bbc40b5ab3fb99a77e21786ddc5abd8d2abec5
Eduardo Pessoa <eduardo@phoenyx.com.br>
2025-12-20 20:22:28 -0300

    Fix: Change vite base to absolute path / to fix assets loading on sub-routes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Permite rodar em subpastas (XAMPP)
+  base: '/', // Permite rodar em subpastas (XAMPP)
   plugins: [react()],
   build: {
     outDir: 'dist',
